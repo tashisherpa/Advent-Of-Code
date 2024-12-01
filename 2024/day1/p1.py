0398413709885788d3cd7cd056cc1cc0a913d368
TashiSherpa <tashisherpa0002@gmail.com>
2024-12-01 13:01:40 -0500

    Day 1 Completed

---
+++ 2024/day1/p1.py
@@ -0,0 +1,19 @@
+from typing import List
+
+list1 = []
+list2 = []
+with open("input.txt", "r") as f:
+    for line in f:
+        values = line.split()
+        list1.append(int(values[0]))
+        list2.append(int(values[1]))
+    
+def p1(list1: List[int], list2: List[int]) -> int:
+    res = 0
+    for i in range(len(list1)):
+        res+= abs(list1[i] - list2[i])
+    return res
+
+list1.sort()
+list2.sort()
+print(p1(list1, list2))
